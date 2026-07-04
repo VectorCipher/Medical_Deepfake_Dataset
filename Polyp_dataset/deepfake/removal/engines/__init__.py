9e06e3aa104eca6f63e7082201d8bf75d9e2dce2
Naitik Pal <naitikpal513@gmail.com>
2026-07-04 12:05:15 +0530

    Remove the sd inpainting model file import

--- Polyp_dataset/deepfake/removal/engines/__init__.py
+++ Polyp_dataset/deepfake/removal/engines/__init__.py
@@ -1,5 +1,4 @@
 from .lama_engine import LamaEngine
-from .sd_engine import SDInpaintEngine
 from .opencv_engine import OpenCVEngine
 
-__all__ = ["LamaEngine", "SDInpaintEngine", "OpenCVEngine"]
+__all__ = ["LamaEngine", "OpenCVEngine"]
